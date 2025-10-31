489a69246bd2dcc32701cbcde3b487b91693d492
jaikersiddharth <jaikersiddharth@email.com>
2025-10-31 13:30:51 +0530

    added interview page

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Agent from '@/components/Agent';
+export default function InterviewPage() {
+return (
+    <div>
+        <Agent userName="you" userId='user1' type="generate"/>
+    </div>
+)
+}
